35385eb966162af04f9b0dfd3282bb85f8554cfe
GuerricSa <guerric.sant@gmail.com>
2025-05-06 18:36:31 +0200

    Fix: start command and minimal endpoint

--- back/index.js
+++ back/index.js
@@ -1,35 +1,52 @@
-// Import des modules nécessaires
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import contactRoutes from './routes/contact.js';
+// // Import des modules nécessaires
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import contactRoutes from './routes/contact.js';
 
-// Chargement des variables d'environnement (.env)
-dotenv.config();
+// // Chargement des variables d'environnement (.env)
+// dotenv.config();
 
-// Création de l'application Express
+// // Création de l'application Express
+// const app = express();
+
+// // Middlewares globaux
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// }));
+// app.use(express.json());     // Parse automatiquement le JSON du body
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, le serveur fonctionne !');
+// })
+
+// // Routes
+// app.use('/api', contactRoutes);
+
+// // Port d'écoute (prend celui de .env ou 5000 par défaut)
+// const PORT = process.env.PORT || 5050;
+
+// // Démarrage du serveur
+// app.listen(PORT, () => {
+//   console.log(`Serveur backend démarré sur http://localhost:${PORT}`);
+// });
+
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
-
-// Middlewares globaux
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-app.use(express.json());     // Parse automatiquement le JSON du body
-
-app.get('/', (req, res) => {
-  res.send('Hello, le serveur fonctionne !');
-})
-
-// Routes
-app.use('/api', contactRoutes);
-
-// Port d'écoute (prend celui de .env ou 5000 par défaut)
 const PORT = process.env.PORT || 5050;
 
-// Démarrage du serveur
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/contact', (req, res) => {
+  res.status(200).json({ message: 'Test réussi !' });
+});
+
 app.listen(PORT, () => {
   console.log(`Serveur backend démarré sur http://localhost:${PORT}`);
 });
